4fc38f1570d84a4bb5f95dd9a6472f597de3edc5
Jarvis <tanmoydey8777@gmail.com>
2023-03-17 01:52:43 +0530

    add: sortable trait

---
+++ src/Sortable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace YoursJarvis\FiltersQuery;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+
+trait Sortable
+{
+    public function scopeSortBy(Builder $builder, $sort_terms = null): Builder
+    {
+        if ($sort_terms) {
+
+            collect($sort_terms)->map(function ($sort_term) use ($builder) {
+                $column = Str::beforeLast($sort_term, ',');
+                $direction = Str::afterLast($sort_term, ',');
+
+                $builder->orderBy($column, $direction);
+            });
+        } else {
+            $builder->latest();
+        }
+
+        return $builder;
+    }
+}
